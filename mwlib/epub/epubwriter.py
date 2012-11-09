aa40e032817cfb1314c8e2d3b0cbf2eca8071ea2
Volker Haas <volker.haas@brainbot.com>
2012-11-09 16:14:25 +0100

    add cover image option

--- mwlib/epub/epubwriter.py
+++ mwlib/epub/epubwriter.py
@@ -41,10 +41,9 @@ class EpubContainer(object):
         self.added_files = set()
         self.write_mime_type()
         self.write_meta_inf()
-
         self.articles =[]
-
         self.coll = coll
+        self.cover_img_path = None
 
     def add_file(self, fn, content, compression=True):
         if fn in self.added_files:
@@ -81,14 +80,18 @@ class EpubContainer(object):
         self.zf.close()
 
     def writeNCX(self):
+        meta = [E.meta({'name': item[0],
+                        'content': item[1]}
+                       ) for item in [("dtb:uid", "123456789X"),
+                                      ("dtb:depth", "1"),
+                                      ("dtb:totalPageCount", '0'),
+                                      ("dtb:maxPageNumber", '0') ]]
+        if self.cover_img_path:
+            meta.append(E.meta(dict(name='cover',
+                                    content='cover-image')))
         tree = E.ncx({'version': '2005-1',
                       'xmlns': 'http://www.daisy.org/z3986/2005/ncx/'},
-                     E.head(*[E.meta({'name': item[0],
-                                      'content': item[1]}
-                                      ) for item in [("dtb:uid", "123456789X"),
-                                                     ("dtb:depth", "1"),
-                                                     ("dtb:totalPageCount", '0'),
-                                                     ("dtb:maxPageNumber", '0') ]]),
+                     E.head(*meta),
                      E.docTitle(E.text(self.coll.title)),
                      E.docAuthor(E.text(self.coll.editor)),
                      )
@@ -150,17 +153,20 @@ class EpubContainer(object):
                                 'image/jpeg',
                                 'image/gif',
                                 ]:
-                    tree.append(E.item({'id': safe_xml_id(fn),
+                    _id = 'cover-image' if fn==self.cover_img_path else safe_xml_id(fn)
+                    tree.append(E.item({'id': _id,
                                         'href': fn,
                                         'media-type': mimetype}))
-
 
             return tree
 
         def writeOPF_spine():
-            tree = E.spine({'toc': 'ncx'},
-                           *[E.itemref(idref=article.id)
-                             for article in self.articles])
+            tree = E.spine({'toc': 'ncx'})
+            for article in self.articles:
+                if article.type == 'cover':
+                    tree.append(E.itemref(idref=article.id, linear="no"))
+                else:
+                    tree.append(E.itemref(idref=article.id))
             return tree
 
         tree = E.package({'version': "2.0",
@@ -173,6 +179,10 @@ class EpubContainer(object):
                      writeOPF_spine()]
                      )
         #FIXME: check if guide section should be written
+        if self.cover_img_path:
+            tree.append(E.guide(
+                E.reference(dict(type='cover', href=self.cover_img_path)
+                )))
         xml = etree.tostring(tree, method='xml', encoding='utf-8',pretty_print=True, xml_declaration=True)
         self.add_file(config.opf_fn, xml)
 
@@ -197,15 +207,29 @@ class EpubContainer(object):
                 zip_fn = os.path.join(config.img_abs_path, basename)
                 self.link_file(img_fn, zip_fn, compression=False)
 
+    def addCover(self, xml, cover_img_path):
+        path = 'OPS/cover.html'
+        self.add_file(path, xml)
+        self.articles.append(ArticleInfo(id='cover',
+                                         path=os.path.basename(path),
+                                         title='cover',
+                                         type='cover',
+                                         ))
+
+        zip_fn = os.path.join(config.img_abs_path, 'cover'+ os.path.splitext(cover_img_path)[1])
+        self.link_file(cover_img_path, zip_fn, compression=False)
+        self.cover_img_path = zip_fn[4:] if zip_fn.startswith('OPS/') else zip_fn
+
 
 class EpubWriter(object):
 
-    def __init__(self, output, coll, status_callback=None):
+    def __init__(self, output, coll, status_callback=None, cover_img=None):
         self.output = output
         self.target_dir = os.path.dirname(output)
         self.coll = coll
         self.scaled_images = {}
         self.status_callback = status_callback
+        self.cover_img = cover_img
 
     def initContainer(self):
         if not os.path.exists(self.target_dir):
@@ -221,6 +245,7 @@ class EpubWriter(object):
     def renderColl(self, dump_xhtml=False):
         xhtml = None
         self.initContainer()
+        self.processCoverImage()
         self.processTitlePage()
         progress_inc = 100.0/len(self.coll.outline.items)
         for n, (lvl, webpage) in enumerate(self.coll.outline.walk()):
@@ -234,6 +259,23 @@ class EpubWriter(object):
         self.closeContainer()
         if dump_xhtml:
             return xhtml
+
+    def processCoverImage(self):
+        if not self.cover_img:
+            return
+        content = [E.div(dict(style='width:100%;height:100%;'),
+                         E.img(dict(src='images/' + 'cover'+ os.path.splitext(self.cover_img)[1],
+                                    alt='',
+                                    #width='100%',
+                                    height='100%',
+                                    style='max-height:100%;max-width:100%;margin:auto;',
+                                    ))),
+                   ]
+        xml = misc.xhtml_page(title='cover',
+                               body_content=content,
+                               flatten=True)
+        self.container.addCover(xml, os.path.abspath(self.cover_img))
+
 
     def processTitlePage(self):
         if not any(txt != '' for txt in [self.coll.title,
@@ -249,7 +291,6 @@ class EpubWriter(object):
                         E.h3(self.coll.editor,
                              style="margin-top:1em;font-size:100%;text-align:center;"),
                         ]
-
         if any('wikipedia.org' in url for url in self.coll.url2webpage):
             img_src = 'wikipedia_logo.jpg'
             titlepage.images = {img_src:
@@ -366,7 +407,8 @@ def render_fragment(epub_fn, fragment, dump_xhtml=False):
 def writer(env, output,
            status_callback=None,
            validate=False,
-           dump_xhtml=False
+           dump_xhtml=False,
+           cover_img=None,
            ):
     if status_callback:
         image_scaling_status = status_callback.getSubRange(1, 65)
@@ -377,7 +419,7 @@ def writer(env, output,
     zipfn = env
     coll = collection.coll_from_zip(tmpdir, zipfn, status_callback=image_scaling_status)
     rendering_status(status='generating epubfile')
-    epub = EpubWriter(output, coll, status_callback=rendering_status)
+    epub = EpubWriter(output, coll, status_callback=rendering_status, cover_img=cover_img)
     epub.renderColl(dump_xhtml=dump_xhtml)
     # import cProfile
     # cProfile.runctx('epub.renderColl(dump_xhtml=dump_xhtml)',globals(), locals(), 'profile')
@@ -409,5 +451,9 @@ writer.file_extension = 'epub'
 writer.options = {
     'dump_xhtml': {
         'help': 'Debugging flag - when set output generated xhtml of all articles',
+        },
+    'cover_img': {
+        'param': 'FILENAME',
+        'help': 'filename of an image for the cover page',
         },
     }
